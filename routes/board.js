8ac180485ec06eb2b2b762f82db6832bbf6d7dea
EunChan Park <pec9399@naver.com>
2018-05-17 15:42:46 +0900

    [Add] Post Edit Routing
    Add PUT '/board/edit/{:id}'

--- routes/board.js
+++ routes/board.js
@@ -1,4 +1,4 @@
-
+//원래 import로 구현해야하지만 ec6가 적용이 안되있어 일단 require로 구현
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
@@ -29,6 +29,29 @@ router.get('/list/?*', async (req, res) => {
 router.post('/write', async (req,res) => {
 	try{
 		const postCreate = await models.Post.create(req.body);
+		
+		res.send({
+			result: true
+		});
+	} catch(e){
+		res.send({
+			result: false
+		});
+	}
+});
+
+//Edit post
+router.put('/edit/:id', async (req,res) => {
+	try{
+		const postUpdate = await models.Post.update(
+			req.body,
+			{
+				where:{
+					id: req.params.id
+				}	
+			}
+		);
+
 		res.send({
 			result: true
 		});
